4f84a82662261079c792be90b946f795af11a970
chaitanyasinghnegi <chaitanyanegi1@gmail.com>
2024-03-23 16:20:29 +0530

    added features

--- task 4/task4.cpp
+++ task 4/task4.cpp
@@ -4,15 +4,50 @@
 
 using namespace std;
 
+struct Task
+{
+    string taskDesc;
+    bool completed;
+};
+
 class ToDoList
 {
 private:
-    vector<string> tasks;
+    vector<Task> tasksList;
 
 public:
     void addTask(const string &task)
     {
-        tasks.push_back(task);
+        tasksList.push_back({task, false});
+    }
+
+    void viewTasks()
+    {
+        cout << endl
+             << "Tasks : " << endl;
+        if (tasksList.empty())
+        {
+            cout << "No tasks remaining" << endl;
+        }
+        else
+        {
+            for (int i = 0; i < tasksList.size(); i++)
+            {
+                cout << i + 1 << ". " << (tasksList[i].completed ? "[Completed] " : "[Pending] ");
+                cout << tasksList[i].taskDesc << endl;
+            }
+        }
+    }
+
+    void taskCompleted(int index)
+    {
+        if (tasksList.size() < index)
+        {
+            cout << "Invalid task index"<<endl;
+        } else {
+            tasksList[index-1].completed=true;
+            cout << "Marked task as completed"<<endl;
+        }
     }
 };
 
